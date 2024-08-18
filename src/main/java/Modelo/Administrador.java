75689167e18da9de50cc82fe4190f7b1cdc0fd9f
Max Saturn <Max Saturn@MAX>
2024-08-18 20:25:01 -0300

    AGREGUE ADM Y USUARIO

---
+++ src/main/java/Modelo/Administrador.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Max Saturn
+ */
+public class Administrador extends Persona {
+    int ID_Administrador;
+    
+}
